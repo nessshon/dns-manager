84e926d84ab054c5fc57092cc3ebbed30c17fb28
ness <shonness98@gmail.com>
2025-10-10 08:47:43 +0500

    first commit

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,14 @@
+import type { ComponentType, JSX } from 'react';
+
+import { IndexPage } from '@/pages/IndexPage/IndexPage';
+
+export interface RouteType {
+  path: string;
+  Component: ComponentType;
+  title?: string;
+  icon?: JSX.Element;
+}
+
+export const routes: RouteType[] = [
+  { path: '/', Component: IndexPage },
+];
